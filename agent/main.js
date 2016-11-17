d7d11b3f098411e7b6db551dca7a153d9d742fb8
larry852 <larryportocarrero@gmail.com>
2016-11-17 16:00:43 -0500

    Se actualiza la informacion del agente en el form

--- agent/main.js
+++ agent/main.js
@@ -10,27 +10,27 @@ function connectServer(ip) {
 	})
 
 		socket.on('money', function(data){
-	    console.log(data); //consola del navegador
+	    console.log('Nueva peticion para el  evento: ' + data); //consola del navegador
 	    sendMoney(data,socket); //parm- nombre del evento
 	})
 
 		socket.on('numSales', function(data){
-	    console.log(data); //consola del navegador
+	    console.log('Nueva peticion para el  evento: ' + data); //consola del navegador
 	    sendNumSales(data,socket);
 	})
 
 		socket.on('timeSale', function(data){
-	    console.log(data); //consola del navegador
+	    console.log('Nueva peticion para el  evento: ' + data); //consola del navegador
 	    sendTimeSale(data,socket);
 	})
 		
 		socket.on('gain', function(data){
-	    console.log(data); //consola del navegador
+	    console.log('Nueva peticion para el  evento: ' + data); //consola del navegador
 	    sendGain(data,socket);
 	})
 
 		socket.on('typeEvent', function(data){
-	    console.log(data); //consola del navegador
+	    console.log('Nueva peticion para el  evento: ' + data); //consola del navegador
 	    sendTypeEvent(data,socket);
 	})
 
@@ -123,3 +123,16 @@ function connectServer(ip) {
 		socket.emit('reply-message',data);
 		return false;
 	}
+
+
+function WriteToFile() {
+
+    set fso = CreateObject("Scripting.FileSystemObject");  
+    set s = fso.CreateTextFile("log.txt", True);
+    s.writeline("HI");
+    s.writeline("Bye");
+    s.writeline("-----------------------------");
+    s.Close();
+ }
+
+WriteToFile();
